2fb95883b97bb30db7b484fb897032ae96b8c85a
erba8 <manyb2308@gmail.com>
2025-07-10 12:19:58 -0400

    c# intro (basic 04)
    you need to do again

--- Assets/Geo_Quest/Scenes/GeoController.cs
+++ Assets/Geo_Quest/Scenes/GeoController.cs
@@ -6,18 +6,37 @@ public class NewBehaviourScript : MonoBehaviour
 {
     string introduction = "Hello!";
     int ver01 = 3;
+    private Rigidbody2D rb;
         // Start is called before the first frame update
     void Start()
     {
+        
+        rb = GetComponent<Rigidbody2D>();
         Debug.Log("Hello Wrold");
         string introduction2 = "Wrold..";
         Debug.Log(introduction + introduction2);
     }
-    
+
     // Update is called once per frame
     void Update()
     {
         Debug.Log(ver01);
         ver01++;
+        rb.velocity = Vector2.left;
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
     }
 }
