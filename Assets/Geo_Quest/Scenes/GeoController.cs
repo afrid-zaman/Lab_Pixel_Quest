799751cd832548ca9a1af202316315ba086ae628
afrid-zaman <afridzaman57@gmail.com>
2025-07-14 12:15:50 -0400

    C3 intro (Basic 04)

--- Assets/Geo_Quest/Scenes/GeoController.cs
+++ Assets/Geo_Quest/Scenes/GeoController.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
+
 {
+    public float changingVelocity;
+    private Rigidbody2D rb;
     string varOne = "Hello";
     int var = 3;
         // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         Debug.Log("Hello World");
         string varTwo = "World";
         Debug.Log("varOne + vartwo");
@@ -17,8 +22,10 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(var);
-        var++;
-        transform.position += new Vector3(0.005f, 0, 0);
+        float inputxaxis = Input.GetAxis("Horizontal");
+        Debug.Log(inputxaxis);
+        rb.velocity = new Vector2(inputxaxis * changingVelocity, rb.velocity.y);
+
+        
     }
 }
