35b720210a41e7255297854de186d9232fc8e47e
afrid-zaman <afridzaman57@gmail.com>
2025-07-15 15:36:51 -0400

    game

---
+++ Assets/Geo_Quest/Scripts/Death.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    string thislvl = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thislvl);
+                    break;
+
+                }
+        }
+    }
+}
---
+++ Assets/Geo_Quest/Scripts/newLvl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class newLvl : MonoBehaviour
+{
+    public string nextLvl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLvl);
+                    break;
+
+                }
+        }
+    }
+}
