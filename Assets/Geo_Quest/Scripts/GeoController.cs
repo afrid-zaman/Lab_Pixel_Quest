7d814037ad6bf061906b447a1ce622dda162ebe1
afrid-zaman <afridzaman57@gmail.com>
2025-07-15 22:14:00 -0400

    aswd movement

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -10,7 +10,7 @@ public class NewBehaviourScript : MonoBehaviour
     private Rigidbody2D rb;
     string varOne = "Hello";
     int var = 3;
-        // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,10 +22,29 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream:Assets/Geo_Quest/Scripts/GeoController.cs
         float inputxaxis = Input.GetAxis("Horizontal");
         Debug.Log(inputxaxis);
         rb.velocity = new Vector2(inputxaxis * changingVelocity, rb.velocity.y);
 
         
+=======
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);       /*/press W = OBJECT moves 01 unit up./*/
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);      //press S = object moves 01 unit down
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A)) 
+        {
+            transform.position += new Vector3(-1, 0, 0); 
+        }
+>>>>>>> Stashed changes:Assets/Geo_Quest/Scenes/GeoController.cs
     }
 }
